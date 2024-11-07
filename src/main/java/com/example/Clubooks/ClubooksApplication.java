b61f378464bf318a32e25af027b0bdebfcb4ecde
Marllon Home <m.w.p.ferreira@outlook.com>
2024-11-07 16:06:38 -0300

    first commit

---
+++ src/main/java/com/example/Clubooks/ClubooksApplication.java
@@ -0,0 +1,13 @@
+package com.example.Clubooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClubooksApplication.class, args);
+	}
+
+}
